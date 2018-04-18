e30e33aee371c1162a4f4cb1fc2b46bb94f1878a
Willy Li <willy@17.media>
2018-04-18 23:33:54 +0800

    Clean old files

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,6 @@
 <template>
   <header class="header">
-    <div class="header__logo">
-      <a href="https://www.cello-cello.com/">
-        <img src="/images/logo.jpg" alt="" />
-      </a>
-    </div>
-    <div class="header__menu">
-      
+    <div class="header__logo">ANO
     </div>
   </header>
 </template>
@@ -17,11 +11,5 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.header {
-  &__logo {
-    & > a {
-      display: block;
-    }
-  }
-}
+
 </style>
