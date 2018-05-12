ee64e0de3af056b99fecf1c1eed5569e7f4ba3cf
ano <ano@anode-MacBook-Pro.local>
2018-05-12 17:58:13 +0800

    Add static resources and update project.js file

--- assets/project.js
+++ assets/project.js
@@ -1,51 +1,115 @@
 export default [{
-        "id": "0",
-        "name": "SCB",
-        "img": "/images/list-scb.png"
-    },
-    {
-        "id": "1",
-        "name": "FET",
-        "img": "/images/list-fenet-event.png"
-    },
-    {
-        "id": "2",
-        "name": "JOMALON",
-        "img": "/images/list-jomalon.png"
-    },
-    {
-        "id": "3",
-        "name": "COR",
-        "img": "/images/list-cor.png"
-    },
-    {
-        "id": "4",
-        "name": "FUBON",
-        "img": "/images/list-fubon.png"
-    },
-    {
-        "id": "5",
-        "name": "UDESIGN",
-        "img": "/images/list-udesign.png"
-    },
-    {
-        "id": "6",
-        "name": "APLUS",
-        "img": "/images/list-aplus.png"
-    },
-    {
-        "id": "7",
-        "name": "DAILY",
-        "img": "/images/list-daily.png"
-    },
-    {
-        "id": "8",
-        "name": "PAYEASY",
-        "img": "/images/list-payeasy.png"
-    },
-    {
-        "id": "9",
-        "name": "YUTING",
-        "img": "/images/list-yuting.png"
+    "id": "0",
+    "name": "SCB",
+    "img": "/images/list-scb.png"
+},
+{
+    "id": "1",
+    "name": "FET",
+    "img": "/images/list-fenet-event.png"
+},
+{
+    "id": "2",
+    "name": "JOMALON",
+    "img": "/images/list-jomalon.png"
+},
+{
+    "id": "3",
+    "name": "COR",
+    "img": "/images/list-cor.png",
+    "data": {
+        "chinese": "遠山呼喚Logo設計",
+        "english": "Calls Over Ridges Logo Design",
+        "year": 2017,
+        "category": "Designer",
+        "status": "Delivery",
+        "desc": "「遠山呼喚」是來自台灣的國際非營利組織，自2015年尼泊爾震後幫助當地孩童重返學校，至今仍持續在尼泊爾協助孩子們透過教育完成脫貧的夢想。在這個專案上結合了組織的核心：小孩、山以及代表希望的氣球，並有各自帶有寓意的黃色、藍色與紅色，並以插畫的方式來述說這個故事。"
     }
+},
+{
+    "id": "4",
+    "name": "FUBON",
+    "img": "/images/list-fubon.png",
+    "data": {
+        "chinese": "富邦產險會員中心網站",
+        "english": "Fubon Insurance Member Center Website",
+        "year": 2017,
+        "category": "Planner / Designer",
+        "status": "Delivery",
+        "desc": "富邦產險會員中心網站是我第一個從提案、資訊架構整理、企劃到設計網站主視覺設計的案子。在這個過程中，試圖以設計師的角度去提出改版的概念，從一個大多以條列方式呈現資料的保險網站，透過以使用者體驗為中心思考，同步進行資訊整理與UI元件設計，全面性的歸納、整理讓資訊更清晰，更易於使用。",
+        "goal": "納入新的資訊內容，並整合個別使用者所需的資訊到新規劃的個人首頁\n將舊有手機版網頁，重新設計為RWD網頁\n從Banner視覺選擇到元件設計，改變傳統產險形象",
+        "plan": "本次改版加入了許多新內容，並包含會員登入功能，並有設置非會員也能瀏覽的頁面。企劃從SiteMap梳理架構開始；Functional Map確認各部分功能；重點功能的Flow Chart規劃；到最後的UI Flow盤點出須產出頁面"
+    }
+},
+{
+    "id": "5",
+    "name": "UDESIGN",
+    "img": "/images/list-udesign.png",
+    "data": {
+        "chinese": "Udesign\n官方網站",
+        "english": "Udesign Offical Website",
+        "year": 2017,
+        "category": "Designer",
+        "status": "Proposal",
+        "desc": "Udesign是一個結合電子商務與設計媒體的文創平台，主要販售文創商品外，亦有集資、品牌推薦並有關注國際設計趨勢文章等複合性質。目前線上販售文創商品的平台眾多，雖然同為電商性質，但希望能夠加強Udesign對於設計潮流敏感的這個特性，因此在專案上透過一點視覺上的巧思，讓這個平台更有獨特與識別性。",
+        "goal": "將舊有電腦版網頁，重新設計為RWD網頁\n商品圖片與廣告共用舊版資料",
+        "highlight": "在商品列表頁，左側可以展開了一個工具列方便使用者篩選商品，亦可以在此狀態下瀏覽商品"
+    }
+},
+{
+    "id": "6",
+    "name": "APLUS",
+    "img": "/images/list-aplus.png",
+    "data": {
+        "chinese": "艾普拉斯官方網站",
+        "english": "Aplus Digital Offical Website",
+        "year": 2016,
+        "category": "Designer",
+        "status": "Proposal",
+        "desc": "Udesign是一個結合電子商務與設計媒體的文創平台，主要販售文創商品外，亦有集資、品牌推薦並有關注國際設計趨勢文章等複合性質。目前線上販售文創商品的平台眾多，雖然同為電商性質，但希望能夠加強Udesign對於設計潮流敏感的這個特性，因此在專案上透過一點視覺上的巧思，讓這個平台更有獨特與識別性。",
+        "goal": "將舊有電腦版網頁，重新設計為RWD網頁\n商品圖片與廣告共用舊版資料",
+        "highlight": "在商品列表頁，左側可以展開了一個工具列方便使用者篩選商品，亦可以在此狀態下瀏覽商品"
+    }
+},
+{
+    "id": "7",
+    "name": "DAILY",
+    "img": "/images/list-daily.png",
+    "data": {
+        "chinese": "Daily UI挑戰",
+        "english": "Daily UI Challenge",
+        "year": "2017 -",
+        "category": "Planner / Designer",
+        "status": "Personal Work",
+    }
+},
+{
+    "id": "8",
+    "name": "PAYEASY",
+    "img": "/images/list-payeasy.png",
+    "data": {
+        "chinese": "Payeasy員工訂房網",
+        "english": "Payeasy Employee Booking Website ",
+        "year": 2017,
+        "category": "Designer",
+        "status": "Delivery",
+        "desc": "Payeasy員工訂房網是一個供其公司內部使用的訂房網站，因訂房網站資訊量與功能眾多，進行專案時比較大的琢磨點是在於整合客戶需求並參照國內外旅遊網站功能來進行開發，朝給予最佳的使用者體驗邁進。",
+        "goal": "將舊有電腦版網頁，重新設計為RWD網頁\n圖片共用舊版資料",
+        "highlight": "每間旅社的頁面上有觀看旅客評論的區塊，由對話框而來的設計概念，為頁面增添輕鬆氛圍"
+    }
+},
+{
+    "id": "9",
+    "name": "YUTING",
+    "img": "/images/list-yuting.png",
+    "data": {
+        "chinese": "鍾宇婷個人網站",
+        "english": "Yu-Ting Chung Personal Website",
+        "year": 2016,
+        "category": "Planner / Designer / Coder / Photographer (Teamwork)",
+        "status": "School Project",
+        "desc": "鍾宇婷是一名來自台北的編劇與導演，在規劃網站前有閱讀過她的劇本、分鏡，也有機會在旁觀察了的影像製作的過程，體驗到從敘述文字化為想像、再轉呈為複雜的影片表現，作品裡有各個創作階段互相交錯的痕跡，進而也影響本次的設計概念，有穿插許多手寫痕跡與不勻稱的排版等，就像散落的文件一般。這個網站設計對我來說特別的是，當需呈現的作品、影像已有一定強度，設計師的角色就要思考如何去引導這些影像被觀看的過程。",
+        "highlight": "以互動的方式來呈現詩文，視覺、文字、動作形成不同的閱讀體驗"
+    }
+}
 ]
